2e031d3e3c76ffb3b4e38d23050f2a776e4f80e1
ARedondoVilla <alexredondovilla@gmail.com>
2020-10-15 15:46:48 +0000

    prueba 4

--- src/main.js
+++ src/main.js
@@ -5,10 +5,22 @@ function onClick() {
     
 }
 
+function porPantalla() {
+    let lista = document.querySelector("#element .list");
+    let li = document.createElement("li");
+    let text = document.createTextNode("Has pulsado el boton");
+
+    li.appendChild(text);
+    lista.appendChild(li);
+    console.log("Click");
+    
+
+}
+
 window.onload = function() {
     let nosequedecir = document.querySelector("#element");
     let ul = document.querySelector(".list");
     let button = nosequedecir.querySelector("button"); 
 
-    button.addEventListener("click", onClick);
+    button.addEventListener("click", porPantalla);
 }   
